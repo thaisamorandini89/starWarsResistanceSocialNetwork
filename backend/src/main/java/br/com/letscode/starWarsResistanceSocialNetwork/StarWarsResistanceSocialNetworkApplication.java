0f2eb9694eaf568886270bcda85d843cd82123ab
Thaisa Honorio Morandini <thaisamorandini@gmail.com>
2021-09-18 19:06:39 -0300

    Project created

---
+++ backend/src/main/java/br/com/letscode/starWarsResistanceSocialNetwork/StarWarsResistanceSocialNetworkApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.starWarsResistanceSocialNetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsResistanceSocialNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarWarsResistanceSocialNetworkApplication.class, args);
+	}
+
+}
