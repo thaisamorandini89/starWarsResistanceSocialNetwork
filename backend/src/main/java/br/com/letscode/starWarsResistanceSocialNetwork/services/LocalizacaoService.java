e4e9140ac6b1792073b930dfaa998de1a247481a
Thaisa Honorio Morandini <thaisamorandini@gmail.com>
2021-09-19 00:35:20 -0300

    Update Localização

--- backend/src/main/java/br/com/letscode/starWarsResistanceSocialNetwork/services/LocalizacaoService.java
+++ backend/src/main/java/br/com/letscode/starWarsResistanceSocialNetwork/services/LocalizacaoService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,16 +13,16 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.letscode.starWarsResistanceSocialNetwork.dto.LocalizacaoDTO;
 import br.com.letscode.starWarsResistanceSocialNetwork.model.Localizacao;
 import br.com.letscode.starWarsResistanceSocialNetwork.repositories.LocalizacaoRepostiory;
-import br.com.letscode.starWarsResistanceSocialNetwork.services.exceptions.EntityNotFoundException;
+import br.com.letscode.starWarsResistanceSocialNetwork.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class LocalizacaoService {
-	
+
 	@Autowired
 	private LocalizacaoRepostiory repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<LocalizacaoDTO> findAll(){
+	public List<LocalizacaoDTO> findAll() {
 		List<Localizacao> list = repository.findAll();
 		return list.stream().map(x -> new LocalizacaoDTO(x)).collect(Collectors.toList());
 	}
@@ -28,11 +30,11 @@ public class LocalizacaoService {
 	@Transactional(readOnly = true)
 	public LocalizacaoDTO findById(Long id) {
 		Optional<Localizacao> obj = repository.findById(id);
-		Localizacao entity = obj.orElseThrow(() -> new EntityNotFoundException("Entidade não encontrada"));
+		Localizacao entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entidade não encontrada"));
 		return new LocalizacaoDTO(entity);
 	}
 
-	@Transactional(readOnly = true)
+	@Transactional
 	public LocalizacaoDTO insert(LocalizacaoDTO dto) {
 		Localizacao entity = new Localizacao();
 		copyToEntity(dto, entity);
@@ -44,5 +46,18 @@ public class LocalizacaoService {
 		entity.setX(dto.getX());
 		entity.setY(dto.getY());
 		entity.setBase(dto.getBase());
+	}
+
+	@Transactional
+	public LocalizacaoDTO update(Long id, LocalizacaoDTO dto) {
+		try {
+			Localizacao entity = repository.getOne(id);
+			copyToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new LocalizacaoDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Entidade não encontrada");
+		}
+
 	}
 }
